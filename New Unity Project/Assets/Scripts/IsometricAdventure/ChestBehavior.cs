34b44996ffce74117b175ca06e2b36db41ff929f
bcea3206 <cgregory8@student.gsu.edu>
2021-09-03 06:33:49 -0400

    first committ

---
+++ New Unity Project/Assets/Scripts/IsometricAdventure/ChestBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestBehavior : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            anim.SetBool("IsOpen", true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            anim.SetBool("IsOpen", false);
+        }
+    }
+
+//Chest animation event handler
+
+
+    public void  ChestEvt()
+    {
+        Debug.Log("The Chest Has Opened");
+    }
+}
+
